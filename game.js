daac7d4cd4e7216309b988aa3927b1e8c5ae55cc
andrzuk <andrzuk@gmail.com>
2024-08-16 19:39:27 +0200

    Bug fix: get possible moves for pawns.

--- game.js
+++ game.js
@@ -1097,10 +1097,14 @@ const rules = {
             potential = [-8, -16, -24, -32, -40, -48, -56, 8, 16, 24, 32, 40, 48, 56, -1, -2, -3, -4, -5, -6, -7, 1, 2, 3, 4, 5, 6, 7, -9, -18, -27, -36, -45, -54, -63, 9, 18, 27, 36, 45, 54, 63, -7, -14, -21, -28, -35, -42, -49, 7, 14, 21, 28, 35, 42, 49];
         }
         if (figure >= 8 && figure < 16) { // black pawn
-            potential = [7, 8, 9, 16];
+            if (this.getPosition(this.getFigureField(figure)).column == 0) potential = [8, 9, 16];
+            else if (this.getPosition(this.getFigureField(figure)).column == 7) potential = [7, 8, 16];
+            else potential = [7, 8, 9, 16];
         }
         if (figure >= 16 && figure < 24) { // white pawn
-            potential = [-7, -8, -9, -16];
+            if (this.getPosition(this.getFigureField(figure)).column == 0) potential = [-7, -8, -16];
+            else if (this.getPosition(this.getFigureField(figure)).column == 7) potential = [-8, -9, -16];
+            else potential = [-7, -8, -9, -16];
         }
         this.fieldOccupancy = fieldOccupancy;
         for (var i = 0; i < potential.length; i++) {
