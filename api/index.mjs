a4002f8c5a5e2750a11109c358d212976f5befaa
andrzuk <andrzuk@gmail.com>
2024-09-05 12:27:41 +0200

    Add lambda function script to API Gateway.

---
+++ api/index.mjs
@@ -0,0 +1,69 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+    DynamoDBDocumentClient,
+    ScanCommand,
+    PutCommand,
+    GetCommand,
+    DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const tableName = "chess";
+
+export const handler = async (event, context) => {
+    let body;
+    let statusCode = 200;
+    const headers = {
+        "Access-Control-Allow-Origin": "https://master.d2mlbkja551bj3.amplifyapp.com",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json",
+    };
+
+    try {
+        switch (event.routeKey) {
+            case "GET /games":
+                body = await dynamo.send(
+                    new ScanCommand({ TableName: tableName })
+                );
+                body = body.Items;
+                if (event.rawQueryString != "") {
+                    body = body.find(item => item.id == event.rawQueryString.replace("id=", ""));
+                }
+                body = { data: body, message: "Loaded successfully." };
+                break;
+            case "POST /games":
+                let requestJSON = JSON.parse(event.body);
+                await dynamo.send(
+                    new PutCommand({
+                        TableName: tableName,
+                        Item: {
+                            id: requestJSON.id,
+                            user: requestJSON.user,
+                            email: requestJSON.email,
+                            sequences: requestJSON.sequences,
+                            details: requestJSON.details,
+                        },
+                    })
+                );
+                body = { data: "Posted id: " + requestJSON.id, message: "Saved successfully." };
+                break;
+            default:
+                throw new Error(`Unsupported route: "${event.routeKey}"`);
+        }
+    } catch (err) {
+        statusCode = 400;
+        body = err.message;
+    } finally {
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers,
+    };
+};
